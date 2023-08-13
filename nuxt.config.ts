4cc6db0028aac053e0a0c1307006263114b6e634
Samuel Ngwarai <samuel.ngwarai@share-now.com>
2023-08-13 22:02:43 +0200

    add robots file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "@nuxtjs/robots"],
   plugins: [{ src: "~/plugins/vercel.ts", mode: "client" }],
   app: {
     head: {
